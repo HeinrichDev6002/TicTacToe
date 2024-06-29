2ac4758fe70d8e0539f12221cb9be5e214c73d3c
HeinrichDev6002 <tutorgames.gmil.com@gmail.com>
2024-06-29 12:45:40 -0300

    feat: implementar jogo da velha

---
+++ src/Toe.java
@@ -0,0 +1,21 @@
+public class Toe {
+   int a = 0;
+    int b = 0;
+    int c = 0;
+    boolean ganhou = false;
+    boolean lose;
+    Toe(){}
+ void Win(){
+        if(a == 1 && b == 1 && c == 1){
+            System.out.println("Ganhou");
+ganhou = true;
+//        } else if(a == 2 && b == 2 && c == 2){
+//            System.out.println("Perdeu");
+//            lose = true;
+//
+        }
+ }
+ void enemy(){
+
+ }
+}
---
+++ src/Window.java
@@ -0,0 +1,111 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Window extends JFrame implements ActionListener {
+    Toe toe = new Toe();
+    JButton button1 = new JButton("Button 1");
+    JButton button2 = new JButton("Button 2");
+    JButton button3 = new JButton("Button 3");
+    JButton button4 = new JButton("Button 4");
+    JButton button5 = new JButton("Button 5");
+    JButton button6 = new JButton("Button 6");
+    JButton button7 = new JButton("Button 7");
+    JButton button8 = new JButton("Button 8");
+    JButton button9 = new JButton("Button 9");
+    Window(){
+        this.setSize(500,500);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.cyan);
+        this.setLayout(new GridLayout(3,3));
+
+
+
+        this.add(button1);
+        this.add(button2);
+        this.add(button3);
+        this.add(button4);
+        this.add(button5);
+        this.add(button6);
+        this.add(button7);
+        this.add(button8);
+        this.add(button9);
+        button1.setFocusable(false);
+        button2.setFocusable(false);
+        button3.setFocusable(false);
+        button4.setFocusable(false);
+        button5.setFocusable(false);
+        button6.setFocusable(false);
+        button7.setFocusable(false);
+        button8.setFocusable(false);
+        button9.setFocusable(false);
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+        button3.addActionListener(this);
+        button4.addActionListener(this);
+        button5.addActionListener(this);
+        button6.addActionListener(this);
+        button7.addActionListener(this);
+        button8.addActionListener(this);
+        button9.addActionListener(this);
+
+        while(toe.ganhou == false){
+        toe.Win();
+        this.setVisible(true);}
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==button1){
+            button1.setEnabled(false);
+            toe.a = 1;
+            button1.setText("X");
+        }
+        if(e.getSource()==button2){
+            button2.setEnabled(false);
+            toe.b = 1;
+            button2.setText("X");
+        }
+        if(e.getSource()==button3){
+            button3.setEnabled(false);
+            toe.c = 1;
+            button3.setText("X");
+        }
+        if(e.getSource()==button4){
+            button4.setEnabled(false);
+            toe.b = 1;
+            button4.setText("X");
+        }
+        if(e.getSource()==button5){
+            button5.setEnabled(false);
+            toe.c = 1;
+            button5.setText("X");
+        }
+        if(e.getSource()==button6){
+            button6.setEnabled(false);
+            toe.a = 1;
+            button6.setText("X");
+        }
+
+        if(e.getSource()==button7){
+            button7.setEnabled(false);
+            toe.c = 1;
+            button7.setText("X");
+        }
+        if(e.getSource()==button8){
+            button8.setEnabled(false);
+            toe.a = 1;
+            button8.setText("X");
+        }
+        if(e.getSource()==button9){
+            button9.setEnabled(false);
+            toe.b = 1;
+            button9.setText("X");
+        }
+
+
+    }
+}
